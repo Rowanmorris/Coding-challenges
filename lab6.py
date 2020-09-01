e63cf283be6071dbce702bbd2dacebbbc3233e79
Rowanmorris <69876289+Rowanmorris@users.noreply.github.com>
2020-09-01 16:15:46 -0700

    update solution

--- lab6.py
+++ lab6.py
@@ -1,17 +1,52 @@
+print('Welcome to the Pig Latin Translator!')
+original_word = input('Enter a word to be translated: \n')
 
-def koa():
-    words = str(input("Input word:")).split()
-    for word in words:
-        print(word[1:] + word[0] + "ay", end = " ")
-    print ()
+
+def koa(original_word):
+    # if no text entered
+    if original_word == "":
+        print('there is nothing there')
+        return
+
+    pyg_latin = 'ay'
+    # splits line into lowercase list
+    word_list = original_word.lower().split()
+    # will treat 'y' as vowel for now
+    vowels = "aeiouy"
+
+    # empty list to hold translated line
+    result = []
+
+    # loop over each word in word list
+    for word in word_list:
+        first = word[0]
+
+        if first in vowels:
+            word += 'w'
+        # example word strong: word = ongstr
+        elif word[1] not in vowels and word[2] not in vowels:
+            word = word[3:] + word[0:3]
+        # example word chase: word = asech
+        elif word[1] not in vowels:
+            word = word[2:] + word[0:2]
+        else:
+            word = word[1:] + word[0]
+
     
-    run_again = input("continue? ")
-    print (run_again)
+        new_word = word + pyg_latin
+    
+        result.append(new_word)
 
-    if run_again == "no" or run_again == "No":
-        print ("ok translator over")
+   
+    translation = " ".join(result)
+    print(translation[0].upper() + translation[1:] + ".")
 
-    elif run_again == "Yes" or run_again == "yes":
-        
-        koa()
-koa()
+    run_again = input('Translate another line? (yes or no): ')
+
+    if run_again == 'yes':
+        original = input('Enter a word:')
+        koa(original_word)
+    elif run_again == 'no':
+        print('k')
+
+koa(original_word)
